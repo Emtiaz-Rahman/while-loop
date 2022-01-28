b86ed39496cd317a0c0c77d08c0aea34ec8841f2
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-01-28 23:57:12 +0600

    while-loop

---
+++ while-loop.js
@@ -0,0 +1,14 @@
+var rostGiven = 0;
+while (rostGiven < 7) {
+    rostGiven++;
+    console.log('rost den please');
+}
+var number = 11;
+while (number < 40) {
+    number = number + 2;
+    console.log(number);
+}
+
+for (var number = 9; number < 40; number += 2) {
+    console.log(number);
+}
